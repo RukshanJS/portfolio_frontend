61e04b7358eec0a3da1c1bf86cc5d1b823eedcdd
rjsenanayaka <rjsenanayaka@gmail.com>
2024-04-28 19:24:57 +0530

    initial

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <ul>
+      <li v-for="project in projects" :key="project.name">
+        <h2>{{ project.name }}</h2>
+        <p>{{ project.description }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const name = 'RUKSHAN JS';
+const projects = [
+  {
+    name: 'Project 1',
+    description: 'This is project 1',
+  },
+  {
+    name: 'Project 2',
+    description: 'This is project 2',
+  },
+];
+</script>
+
+<style>
+</style>

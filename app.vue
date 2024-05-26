1b6a9128777fca0f9926da2459a0762aad748b17
RukshanJS <rjsenanayaka@gmail.com>
2024-05-26 18:03:28 +0530

    chore:

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
+  <h1>{{ name }}</h1>
   <div>
-    <h1>{{ name }}</h1>
+    <h1>PROJECTS</h1>
     <div v-if="pending">Loading...</div>
     <ul>
       <li v-for="project in projects" :key="project.name">
@@ -9,11 +10,23 @@
       </li>
     </ul>
   </div>
+
+  <div>
+    <h1>BLOGS</h1>
+    <div v-if="pending2">Loading...</div>
+    <ul>
+      <li v-for="blog in blogs" :key="blog.title">
+        <h2>{{ blog.title }}</h2>
+        <p>{{ blog.content }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
 const name = 'RUKSHAN JS';
 const { data: projects, pending, error } = useFetch('http://localhost:5000/projects');
+const { data: blogs, pending2, error2 } = useFetch('http://localhost:5000/blogs');
 </script>
 
 <style></style>
